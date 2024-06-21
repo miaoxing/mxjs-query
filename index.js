246fff4061461c6d35341936ef9a861ebc94e66d
twinh <twinhuang@qq.com>
2024-06-21 23:20:12 +0800

    feat(query): 增加 `useQuery` 方法

---
+++ index.js
@@ -0,0 +1,29 @@
+import useSWR from 'swr';
+import $ from 'miaoxing';
+
+const useQuery = (key, config) => {
+  const result = useSWR(key, async () => {
+    const res = await $.http(key);
+
+    const ret = res.ret;
+    if (ret.isErr()) {
+      let error = new Error(ret.message);
+      error.code = ret.code;
+      throw error;
+    }
+
+    // 允许每次请求后对数据进行处理，可以减少重复渲染
+    config?.onLoad && await config.onLoad(ret);
+
+    return ret;
+  }, config);
+
+  // 增加 ret 键名，方便调用
+  result.ret = result.data;
+  const data = result.ret?.data;
+
+  // 更换 data 为接口的 data
+  return { ...result, data };
+};
+
+export { useQuery };
